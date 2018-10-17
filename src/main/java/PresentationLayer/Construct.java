c097fd18109f96c8f148ae3d9082992ab82b1786
Lasse <l.cph@hotmail.com>
2018-10-17 11:46:57 +0200

    Exception added

--- src/main/java/PresentationLayer/Construct.java
+++ src/main/java/PresentationLayer/Construct.java
@@ -14,6 +14,9 @@ public class Construct extends Command {
         int length = Integer.parseInt(request.getParameter("length"));
         int height = Integer.parseInt(request.getParameter("height"));
         HashMap<String, Integer> blockHolder = LogicFacade.createBlockHolder(length, width, height);
+        request.getSession().setAttribute("length", length);
+        request.getSession().setAttribute("width", width);
+        request.getSession().setAttribute("height", height);
         request.getSession().setAttribute("2x4", blockHolder.get("2x4"));
         request.getSession().setAttribute("2x2", blockHolder.get("2x2"));
         request.getSession().setAttribute("1x2", blockHolder.get("1x2"));
